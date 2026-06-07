17520ec294fb6ff4881eb7978e891092ecbd89d4
leeaash <78188599+leeaash@users.noreply.github.com>
2026-06-07 15:59:50 +0800

    Enhance Demry VRF component with climate control features
    Added climate control definitions and updated DemryVrfClimate class.

--- components/demry_vrf/demry_vrf_component.h
+++ components/demry_vrf/demry_vrf_component.h
@@ -1,40 +1,46 @@
 #pragma once
 
-namespace esphome {
-  namespace demry_vrf {
-    class DemryVrfClimate; // Forward declaration
-  }
-}
-#include "demry_vrf_climate.h"
-
-#include "esphome.h"
 #include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
 #include "esphome/components/climate/climate.h"
-
-
-#define TAG "demry.vrf"
+#include "esphome/components/uart/uart.h"
 
 namespace esphome {
   namespace demry_vrf {
-    class DemryVrfComponent : public PollingComponent, public uart::UARTDevice {
-    public:
-      DemryVrfComponent(uart::UARTComponent *uartComponent) : PollingComponent(2000), UARTDevice(uartComponent) {
-        this->uart_ = uartComponent;
-      };
+
+    #define CMD_IDX_ON_OFF 2
+    #define CMD_IDX_CLIMATE_MODE 5
+    #define CMD_IDX_FAN_MODE 7
+    #define CMD_IDX_TARGET_TEMP 4
+
+    #define CMD_ON 0x01
+    #define CMD_OFF 0x00
+
+    #define CMD_CLIMATE_MODE_COOL 0x00
+    #define CMD_CLIMATE_MODE_HEAT 0x01
+    #define CMD_CLIMATE_MODE_FAN_ONLY 0x02
+    #define CMD_CLIMATE_MODE_DRY 0x03
+
+    #define CMD_FAN_MODE_HIGH 0x03
+    #define CMD_FAN_MODE_MEDIUM 0x02
+    #define CMD_FAN_MODE_LOW 0x01
+    #define CMD_FAN_MODE_AUTO 0x00
+
+    class DemryVrfClimate : public climate::Climate, public Component {
+     public:
       void setup() override;
-      void update() override;
-      void loop() override;
-      void register_climate(demry_vrf::DemryVrfClimate *obj) {
-        this->climates_.push_back(obj);
-      }
-      uart::UARTComponent* get_uart() {
-        return this->uart_;
-      }
-    protected:
-      uart::UARTComponent* uart_;
-      std::vector<demry_vrf::DemryVrfClimate *> climates_;
-      uint8_t update_pos_;
+      void control(const climate::ClimateCall &call) override;
+      climate::ClimateTraits traits() override;
+      
+      void set_idx(uint8_t idx) { this->idx_ = idx; }
+      // 💡 关键修改：暴露物理 ID 接口
+      uint8_t get_idx() const { return this->idx_; } 
+      
+      void set_uart(uart::UARTDevice *uart) { this->uart_ = uart; }
+
+     protected:
+      uint8_t idx_;
+      uart::UARTDevice *uart_;
     };
+
   }
 }
